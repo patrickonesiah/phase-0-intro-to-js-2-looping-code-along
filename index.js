e3aff5eaf6ae57356fe82099bb174fa1784de87f
patrickonesiah <patrickonesiah@gmail.com>
2023-09-15 10:26:10 +1000

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+  function writeCards(stringOfNames,eventName) {
+    const newCards = [];
+    for (let i = 0; i < stringOfNames.length; i++) {
+      //console.log(`Thank you, ${stringOfNames[i]}, for the wonderful ${eventName} gift!`);
+      newCards.push(`Thank you, ${stringOfNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+  
+    return newCards;
+  }
+
+function countDown(counter){
+    while (0 <= counter) {
+        console.log(counter--);
+    }
+}
